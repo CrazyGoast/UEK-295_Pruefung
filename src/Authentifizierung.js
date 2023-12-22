8c58be3d5cfd732dd176097ba7735aae212a3a42
Adrian Bischoff <abischoff8307@gmail.com>
2023-12-22 14:53:46 +0000

    Addet a description in Authentifizierung.js. 22.12.2023 15:53

--- src/Authentifizierung.js
+++ src/Authentifizierung.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 
+// Express Router erstellen
 const router = express.Router();
 
+// Middleware einrichten: JSON-Parser und Sitzungsverwaltung
 router.use(express.json());
 router.use(session({
   secret: 'supersecret',
@@ -11,6 +13,7 @@ router.use(session({
   cookie: {},
 }));
 
+// Überprprüft ob das Passwort m295 ist. Wenn nicht wird "Invalid credentials" ausgegeben.
 router.post('/login', (request, response) => {
   const { email, password } = request.body;
   if (password === 'm295') {
@@ -20,19 +23,21 @@ router.post('/login', (request, response) => {
   return response.status(401).json({ error: 'Invalid credentials.' });
 });
 
+// Hier wird überprüft ob man schon eingelogt ist, wenn ja
+// wird die vorhanden, E-Mail als Antwort senden.
+
 router.get('/verify', (request, response) => {
   // Check if email is set in session
   if (request.session.email) {
     return response.status(200).json({ email: request.session.email });
   }
-
+// Fals man nicht eingelogt ist wird ausgegeben " Not logged in".
   return response.status(401).json({ error: 'Not logged in' });
 });
 
+// Mit /logout wird die E-Mail gelöscht und es wird  "Logged out successfully" ausgegeben.
 router.delete('/logout', (request, response) => {
-  // Check if email is set in session
   if (request.session.email) {
-    // Reset link of session to email
     request.session.email = null;
 
     return response.status(200).send('Logged out successfully.');
@@ -41,4 +46,5 @@ router.delete('/logout', (request, response) => {
   return response.status(401).json({ error: 'Not logged in' });
 });
 
+// Export des Router-Objekts für die Verwendung in anderen Dateien
 module.exports = router;
