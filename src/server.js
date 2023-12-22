9d6a407ddc29d93423d5a6cea8b28ac7b2f48f46
Adrian Bischoff <abischoff8307@gmail.com>
2023-12-22 14:40:57 +0000

    Addet a header and description to the programm. 22.12.2023 15:40 Uhr

--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
+//#############################################################################################################
+//    Titel: In diesem Programm geht es darum, dass man einen Task in einem Array speichern kann und ihn      #
+//           bearbeiten und auch wieder löschen. Wenn man will kann man auch die einzelnen Task mit ihrer     #
+//           zugewiesenen Id aufrufen.                                                                        #
+//                                                                                                            #
+//    Autor: Adrian Bischoff | abischoff8307@gmail.com                                                        #
+//                                                                                                            #
+//    Erstellungsdatum: 22.12.2023 09:00 Uhr                                                                  #
+//                                                                                                            #
+//    Erfüllungsdatum:  22.12.2023 16:45 Uhr                                                                  #
+//                                                                                                            #
+//#############################################################################################################
+
+
+
 const express = require('express');
 
 const app = express();
